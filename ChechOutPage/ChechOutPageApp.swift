c462ebb34e8cb28460b2d6a524aa49915d7943e4
Horacio Mota <horaciomota93u@gmail.com>
2023-10-14 13:47:38 -0700

    Initial Commit

---
+++ ChechOutPage/ChechOutPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChechOutPageApp.swift
+//  ChechOutPage
+//
+//  Created by Horacio Mota on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChechOutPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
