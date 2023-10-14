51c9c539d2907793134ae626be840c3305b9684a
Horacio Mota <horaciomota93u@gmail.com>
2023-10-14 15:36:19 -0700

    Payment page finished

--- ChechOutPage/ContentView.swift
+++ ChechOutPage/ContentView.swift
@@ -6,13 +6,13 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
         ZStack() {
             Color.green.ignoresSafeArea(.all)
             VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
+                    Spacer()
                     HStack {
                         Image(systemName: "checkmark")
                             .padding()
@@ -21,6 +21,7 @@ struct ContentView: View {
                         VStack(alignment: .leading)  {
                             Text("Payment completed!")
                                 .font(.headline)
+                                .fontWeight(.bold)
                             Text("Thank you, your payment was successfully completed!")
                                 .font(.subheadline)
                                 .lineLimit(nil)
@@ -29,22 +30,95 @@ struct ContentView: View {
                         .foregroundColor(.white)
                     }
                     .padding(.horizontal)
+                    .padding(.bottom)
                     
                     ZStack {
                         Rectangle()
                             .foregroundColor(Color.white)
+                            .frame(height: 500)
+                            .cornerRadius(20)
                         
                         VStack(alignment: .leading) {
-                            Text("Payment details")
-                                .font(.title)
-                                .fontWeight(.bold)
-                                .multilineTextAlignment(.leading) // Alinhe o texto à esquerda aqui
+                            HStack() {
+                                Text("Payment details")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding(.bottom, 2)
+                                
+                                Image(systemName: "square.and.arrow.up")
+                                    .foregroundColor(.gray)
+
+                            }                                    .padding(.top, -32)
+
                             
-                            Text("Amount")
-                            Text("$18.47")
+                            VStack(alignment: .leading) {
+                                Text("Amount")
+                                    .foregroundColor(.gray)
+                                Text("$18.47")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 2)
+                            
+                            VStack(alignment: .leading) {
+                                Text("CIB Bonuses")
+                                    .foregroundColor(.gray)
+                                Text("17")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 2)
+
+                            VStack(alignment: .leading) {
+                                Text("Date on time")
+                                    .foregroundColor(.gray)
+                                Text("14/10/2023")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 2)
+
+                            VStack(alignment: .leading) {
+                                Text("Payment source")
+                                    .foregroundColor(.gray)
+                                Text("Umico, *1032")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 2)
+
+                            VStack(alignment: .leading) {
+                                Text("Payment Type")
+                                    .foregroundColor(.gray)
+                                Text("common")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 2)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Subscriber code")
+                                    .foregroundColor(.gray)
+                                Text("01000144598901")
+                                    .font(.headline)
+                            }
+                            .padding(.bottom, 20)
+
+                            Button {
+                                //print
+                            } label: {
+                                HStack {
+                                    Image(systemName: "star")
+                                    Text("Add to Templates")
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.blue)
+                                .foregroundColor(.white)
+                            }
+
                         }
+                        .padding(.horizontal)
                         .background(Color.white)
                     }
+                    Spacer()
+
                 }
             }
         }
